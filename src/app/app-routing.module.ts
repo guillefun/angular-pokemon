ce1a9f6b44bd097c6e6779a97e02568bc94c56f5
Guillermo Fernández-Campoamor Fernández <57231808+guillefun@users.noreply.github.com>
2022-11-01 00:21:34 +0100

    update routine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { PokedexModule } from './pages/pokedex/pokedex.module';
 
 const routes: Routes = [{
-  path: '',
+  path: 'pokedex',
   loadChildren: () => import('./pages/pokedex/pokedex.module').then((m) => m.PokedexModule),
-}
+},
+{ path: '**', redirectTo: 'pokedex', pathMatch: 'full' },
 ];
 
 @NgModule({
